871673b6de49d062610539f7bd7db0ea3d03230b
Tudor Iordan <tiordan@bellarmine.edu>
2024-11-12 15:59:25 -0500

    Commit

---
+++ CS131Lab4_TI/src/BadFileName.java
@@ -0,0 +1,10 @@
+//IO TestClass for bad file names
+public class BadFileName{
+  
+  public static void main(String[] args){
+    
+    FileProcessor fp = new FileProcessor("String1.txt",10);
+		fp.processFile();
+  }//end main
+  
+}//end class
---
+++ CS131Lab4_TI/src/BadString.java
@@ -0,0 +1,10 @@
+public class BadString{
+  
+  public static void main(String[] args){
+    	
+    	FileProcessor fp = new FileProcessor("BadString.txt",10);
+		  fp.processFile();
+    
+  }//end main
+  
+}//end class
